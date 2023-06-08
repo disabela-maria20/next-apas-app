962fb2232c08af404dc85ee08f82e75e908e8dee
Isabela Maria <isabela10014@hotmail.com>
2023-06-08 16:41:38 -0300

    feat: :sparkles: add event map logiaca

--- src/components/molecules/stage/index.tsx
+++ src/components/molecules/stage/index.tsx
@@ -1,56 +1,91 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import Styled from './Stage.module.scss'
 
+const COLOR: string[] = ['#36b436', '#912020', '#465b8a', '#8d026f']
+const TEXT: string[] = ['Texto 1', 'Texto 2', 'Texto 3', 'Texto 4']
 const Stage = () => {
   const arrStage = useRef<HTMLDivElement>(null)
+  const circle = useRef<SVGSVGElement>(null)
+
+  const [componente, setComponente] = useState<any>('Texto 1')
+
   useEffect(() => {
-    const circle = arrStage.current?.querySelector('circle')
+    const circleSvg = circle.current?.querySelector('circle')
+    const triangleSvg = arrStage.current?.querySelectorAll('polygon')
 
-    arrStage.current
-      ?.querySelectorAll('polygon')
-      .forEach((item, index, list) => {
-        console.log(list)
-        list[index].addEventListener('mouseover', (e: MouseEvent): void => {
-          const elem = e.target as HTMLDivElement
-        })
+    const activateHover = (index: number): void => {
+      triangleSvg?.forEach((item, i) => {
+        if (i === index) {
+          circleSvg?.setAttribute('fill', COLOR[i])
+          console.log(TEXT[i])
+          setComponente(TEXT[i])
+        }
       })
+    }
+
+    const handleMouseOver = (index: number) => (): void => {
+      activateHover(index)
+    }
+
+    triangleSvg?.forEach((item: SVGPolygonElement, index: number) => {
+      item.addEventListener('mouseover', handleMouseOver(index))
+    })
+
+    return () => {
+      triangleSvg?.forEach((item: SVGPolygonElement, index: number) => {
+        item.removeEventListener('mouseover', handleMouseOver(index))
+      })
+    }
   }, [])
+
   return (
-    <div className={Styled.containerStage}>
-      <div className={Styled.containerBox}>
-        <div className={Styled.blockStage} ref={arrStage}>
-          <span>
-            <svg width="220" height="220" className={Styled.top}>
-              <polygon points="55,55 220,0 110,220" />
-            </svg>
-          </span>
+    <>
+      <div className={Styled.containerStage}>
+        <div className={Styled.containerBox}>
+          <div className={Styled.blockStage} ref={arrStage}>
+            <span className={Styled.top}>
+              <svg width="220" height="220">
+                <polygon points="55,55 220,0 110,220" />
+              </svg>
+              <p>Lorem ipsum dolor sit</p>
+            </span>
 
-          <span>
-            <svg width="220" height="220" className={Styled.botton}>
-              <polygon points="55,55 220,0 110,220" />
-            </svg>
-          </span>
+            <span className={Styled.botton}>
+              <svg width="220" height="220">
+                <polygon points="55,55 220,0 110,220" />
+              </svg>
+              <p>Lorem ipsum dolor sit</p>
+            </span>
 
-          <span>
-            <svg width="220" height="220" className={Styled.right}>
-              <polygon points="55,55 220,0 110,220" />
-            </svg>
-          </span>
+            <span className={Styled.right}>
+              <svg width="220" height="220">
+                <polygon points="55,55 220,0 110,220" />
+              </svg>
+              <p>Lorem ipsum dolor sit</p>
+            </span>
 
-          <span>
-            <svg width="220" height="220" className={Styled.left}>
-              <polygon points="55,55 220,0 110,220" />
-            </svg>
-          </span>
-          <span>
-            <svg width="100" height="100" className={Styled.circle}>
-              <circle cx="45" cy="45" r="45" />
-            </svg>
-          </span>
+            <span className={Styled.left}>
+              <svg width="220" height="220">
+                <polygon points="55,55 220,0 110,220" />
+              </svg>
+              <p>Lorem ipsum dolor sit</p>
+            </span>
+            <span>
+              <svg
+                width="100"
+                height="100"
+                className={Styled.circle}
+                ref={circle}
+              >
+                <circle cx="45" cy="45" r="45" />
+              </svg>
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+      {componente}
+    </>
   )
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,5 @@ import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   const test: string = 'amarelo'
-  console.log(test)
   return <Component {...pageProps} />
 }
