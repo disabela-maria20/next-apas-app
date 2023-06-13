2dc2528333864c9586875e6a934826f5cb63a36e
Isabela Maria <isabela10014@hotmail.com>
2023-06-13 17:36:14 -0300

    feat: :sparkles: add header desktop

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -8,21 +8,30 @@ const Header = () => {
       <Menu />
       <div className={Style.container}>
         <div className={Style.headerBlock}>
+          <div>
+            <img
+              src="./images/logo-header.webp"
+              alt="logo promo"
+              width={318}
+              height={239}
+            />
+            <h1>
+              <span>O EVENTO DE</span>
+              TECNOLOGIA E INOVAÇÃO {''}
+              <span>EXCLUSIVO</span>
+              <p className={Style.subTitle}>
+                DO SETOR <br /> SUPERMERCADISTA
+              </p>
+            </h1>
+            <span className={Style.date}>15-08-2023</span>
+          </div>
           <img
-            src="./images/logo-header.webp"
-            alt="logo promo"
-            width={318}
-            height={239}
+            src="./images/header-illustration.webp"
+            alt="ilustração mercado"
+            width={1883}
+            height={1523}
+            className={Style.Ilus}
           />
-          <h1>
-            <span>O EVENTO DE</span>
-            TECNOLOGIA E INOVAÇÃO {''}
-            <span>EXCLUSIVO</span>
-            <p className={Style.subTitle}>
-              DO SETOR <br /> SUPERMERCADISTA
-            </p>
-          </h1>
-          <span className={Style.date}>15-08-2023</span>
         </div>
       </div>
     </header>
