aa8aad3160ba7a02af7a09a9b6c8e2efc0617fdb
Isabela <isabela10014@hotmail.com>
2023-07-05 18:07:28 -0300

    feat: :ambulance: edit form

--- src/components/organisms/FAQ/FAQ.schema.ts
+++ src/components/organisms/FAQ/FAQ.schema.ts
@@ -25,31 +25,11 @@ export const contactFormSchema = z.object({
         message: 'Endereço de e-mail invalido'
       }
     ),
-  tel: z
-    .string()
-    .min(11, { message: 'Número de telefone invalido' })
-    .nonempty('Telefone é obrigatorio'),
-  // .refine((value: string) => /^\(\d{2}\)\s\d{5}-\d{4}$/.test(value), {
-  //   message: 'Número de telefone invalido'
-  // }),
-  office: z
-    .string()
-    .min(3, { message: 'A qualidade de caracteres minimas é 3' })
-    .nonempty('Infome o cargo'),
-  company: z
-    .string()
-    .nonempty('O nome da empresa é obrigatório')
-    .min(3, { message: 'A qualidade de caracteres minimas é 3' }),
-  cnpj: z
-    .string()
-    .nonempty('O cnpj é obrigatório')
-    .min(14, { message: 'A qualidade de caracteres minimas é 14' })
-    .refine(
-      (value: string) => /^\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}$/.test(value),
-      {
-        message: 'Infome o CNPJ valido'
-      }
-    ),
+  tel: z.string().optional(),
+  office: z.string().optional(),
+  company: z.string().optional(),
+  cnpj: z.string().optional(),
+
   message: z
     .string()
     .min(3, { message: 'A qualidade de caracteres minimas é 5' })
