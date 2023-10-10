8d89072ba90784a6fca2bb05a1736fd37bba9315
Isabela Maria <isabela10014@hotmail.com>
2023-10-10 18:11:44 -0300

    feat: :zap: add galeria

---
+++ src/components/organisms/Gallery/index.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+
+import Style from './Gallery.module.scss'
+import 'react-lazy-load-image-component/src/effects/blur.css'
+
+const Index: React.FC = () => {
+  const [numbers, setNumbers] = useState<string[]>([])
+  const [count, setCount] = useState(8)
+
+  useEffect(() => {
+    const formattedNumbers: string[] = []
+    for (let i = 1; i <= count; i++) {
+      formattedNumbers.push(String(i).padStart(3, '0'))
+    }
+    setNumbers(formattedNumbers)
+  }, [count])
+
+  const handleClick = () => {
+    setCount((prevCount) => prevCount + 10)
+  }
+
+  return (
+    <section className={Style.gallery}>
+      <div className={Style.container}>
+        <h1>Fotos</h1>
+        <div className={Style.grid}>
+          {numbers.map((number, index) => (
+            <LazyLoadImage
+              effect="blur"
+              src={`/images/event/Image_${number}.webp`}
+              key={index}
+            />
+          ))}
+        </div>
+        {count < 131 && <button onClick={handleClick}>Mostrar +</button>}
+      </div>
+    </section>
+  )
+}
+
+export default Index
---
+++ src/pages/galeria.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import-helpers/order-imports */
+import { Menu } from '@/components/molecules'
+import { Footer, Gallery } from '@/components/organisms'
+
+
+const index = () => {
+  return (
+    <>
+      <Menu />
+      <Gallery/>
+      <Footer />
+    </>
+  )
+}
+
+export default index
